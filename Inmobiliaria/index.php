e56311962838abd7abdc5e5df6c2efa34cb34880
Jesus Castellanos <castellanosjesus098@gmail.com>
2026-04-18 21:15:40 -0500

    added little page

--- Inmobiliaria/index.php
+++ Inmobiliaria/index.php
@@ -2,9 +2,10 @@
 include("includes/conexion.php");
 
 // Cargar propiedades destacadas
-$sql = "SELECT i.*, inv.tipo as tipo_operacion, inv.precio as precio_publicado 
+$sql = "SELECT i.*, inv.tipo as tipo_operacion, inv.precio as precio_publicado, inv.fotos, a.telefono as agente_telefono 
         FROM inmuebles i 
         LEFT JOIN inventario inv ON i.id_inmueble = inv.id_inmueble 
+        LEFT JOIN agentes a ON i.id_agente = a.id_agente 
         WHERE i.estado = 'disponible' AND inv.estado = 'activo' 
         ORDER BY inv.fecha_publicacion DESC LIMIT 6";
 $propiedades = mysqli_query($conex, $sql);
